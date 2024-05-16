ed50506e09c0d76a3355fe0eebc96f6e6729743f
BlackSardes <161069298+BlackSardes@users.noreply.github.com>
2024-05-16 20:10:56 -0300

    As variaveis de jõao
    só declarei as variaveis e carreguei o valor na memória do resultado dos nums

--- LetraC_IH.asm
+++ LetraC_IH.asm
@@ -23,6 +23,8 @@ add x29, x29, x31 #salva 8x + 2x em x29
 # Soma o valor da dezena e unidade de A
 add x19, x29, x30
 
+sw x19, va
+
 lb x20, 1025(x0) # sinal de B
 lb x29, 1025(x0) # Decimal de B
 lb x30, 1025(x0) # Unidade de B
@@ -37,6 +39,8 @@ add x29, x29, x31 #salva 8x + 2x em x29
 
 # Soma o valor da dezena e unidade de B
 add x21, x29, x30
+
+sw x21, vb
 
 lb x22, 1025(x0) # sinal de C
 lb x29, 1025(x0) # Decimal de C
@@ -53,9 +57,13 @@ add x29, x29, x31 #salva 8x + 2x em x29
 # Soma o valor da dezena e unidade de C
 add x23, x29, x30
 
+sw x23, vc
+
 # Terminou de ler
 # x = 0
 add x24, x0, x0
+
+sw x24, vx
 
 # Comparação de a
 addi x5, x0, 5 
@@ -81,8 +89,15 @@ Final_Comp:
 
 addi x24, x0, 1
 
+sw x24, vx
+
 # Mostrar na tela
 Else: 
 	addi x24, x24, 48
 	sb x24, 1024(x0)
 halt
+
+va : .word 0x00
+vb : .word 0x00
+vc : .word 0x00
+vx : .word 0x00
