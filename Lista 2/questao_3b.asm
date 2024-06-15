a4022f69065d8894da26d12ef725eb42faa3dc15
João Victor <98539736+jambis-prg@users.noreply.github.com>
2024-06-15 18:08:02 -0300

    Código otimizado
    Diminui o numero de instruções e organizei melhor o código com menos desvios e melhor legibilidade

--- Lista 2/questao_3b.asm
+++ Lista 2/questao_3b.asm
@@ -5,105 +5,104 @@
 addi x26, x0, 0  # Inicializa contador
 addi x27, x0, 1  # Limite do loop
 
-get_num:
-    beq x26, x27, espaco # se x26 == 1 então pega o espaço
-	jal x0, digitos
+L0:
+    lb x7, 1025(x0) # digito 1
+    lb x28, 1025(x0) # digito 2
+    lb x29, 1025(x0) # digito 3
+    lb x30, 1025(x0) # digito 4
+    lb x31, 1025(x0) # digito 5
 
-    espaco:
-        lb x7, 1025(x0) # espaço
-
-    digitos:
-        lb x7, 1025(x0) # digito 1
-        lb x28, 1025(x0) # digito 2
-        lb x29, 1025(x0) # digito 3
-        lb x30, 1025(x0) # digito 4
-        lb x31, 1025(x0) # digito 5
-
-    # Conversão ascii p/ inteiro
-    ascii:
-        addi x7, x7, -48
-        addi x28, x28, -48
-        addi x29, x29, -48
-        addi x30, x30, -48
-        addi x31, x31, -48
+    #Convertendo os digitos de caracter para numeros
+    addi x7, x7, -48
+    addi x28, x28, -48
+    addi x29, x29, -48
+    addi x30, x30, -48
+    addi x31, x31, -48
 
     # DIGITO 5 * 10000:
-    digito_5:
-        addi x12, x0, 1250
-        slli x12, x12, 3 # Carregando 10000 no parametro a de Mul
+    addi x12, x0, 1250
+    slli x12, x12, 3 # Carregando 10000 no parametro a de Mul
 
-        # Carregando digito 1 no parametro b de Mul
-        addi x13, x7, 0
+    # Carregando digito 1 no parametro b de Mul
+    addi x13, x7, 0
 
-        # Faz a multiplicação
-        # E soma no resultado
-        jal x1, Mul
-        addi x18, x10, 0
+    # Faz a multiplicação
+    # E soma no resultado
+    jal x1, Mul
+    addi x18, x10, 0
 
     # DIGITO 4 * 1000:
-    digito_4:
-        addi x12, x0, 1000
-        add x13, x0, x28
+    addi x12, x0, 1000
+    add x13, x0, x28
 
-        jal x1, Mul
-        add x18, x18, x10
+    jal x1, Mul
+    add x18, x18, x10
 
     # DIGITO 3 * 100:
-    digito_3:
-        addi x12, x0, 100
-        add x13, x0, x29
+    addi x12, x0, 100
+    add x13, x0, x29
 
-        jal x1, Mul
-        add x18, x18, x10
+    jal x1, Mul
+    add x18, x18, x10
 
     # DIGITO 2 * 10
-    digito_2:
-        addi x12, x0, 10
-        add x13, x0, x30
+    addi x12, x0, 10
+    add x13, x0, x30
 
-        jal x1, Mul
-        add x18, x18, x10
+    jal x1, Mul
+    add x18, x18, x10
 
     # DIGITO 1 * 1
-    digito_1:
-        add x18, x18, x31
+    add x18, x18, x31
 
-    # RESULTADOS de num1 e num2
-    salva:
-        beq x26, x0, salva_num1
+    bge x26, x27, L1 # if (i >= 1) break;
 
-    salva_num2:
-        addi x13, x18, 0
-        jal x1, mult # calcular num1 * num2
+    addi x20, x18, 0 # Salva o num1 em x20
+    addi x26, x26, 1 # i++;
+    lb x7, 1025(x0) # Carrega caracter espaço para descarte
 
-    salva_num1:
-        addi x20, x18, 0 # salva o num1
-        addi x26, x26, 1 # x26++
-        jal x0, get_num
+    beq x0, x0, L0 # while(true)
 
-mult:
-    add x12, x20, x0 # Salva num1 em x12
-    jal x1, Mul
-	jal x0, result_digits
+L1:
+    addi x12, x20, 0 # Salva num1 em x12
+    addi x13, x18, 0 # Salva num2 em x13
+    jal x1, Mul # num1 * num2
+ 
+    blt x10, x0, L3 # if (x10 <= 0) then go to L3
+    beq x10, x0, L3
 
-fim:
-	addi x19, x19, -1 # correção
-	
-	print:
-		addi x20, x0, -1 # condição de parada
-		beq x19, x20, exit # if cont == -1 exit
-	
-		slli x6, x19, 3 # x6 = cont * 8
-    		add x18, x5, x6 # address of arr[i]
-    
-		lw x10, 0(x18) # x10 = arr[i]
-		sb x10, 1024(x0) # printa
-    
-		addi x19, x19, -1 # cont++
-		jal x0, print
+    lw x19, result_str_ptr # array p/ os digitos
+    addi x20, x0, 0 # cont
+    addi x21, x10, 0 # temp
+    addi x5, x0, 0 # digitos
 
-exit:
-	halt
+    # while temp > 0
+    L2:
+        addi x11, x21, 0 # Div_By_Ten(temp);
+        jal x1, Div_By_Ten
+
+        addi x21, x10, 0 # temp = Div_By_Ten(temp).first;
+        addi x5, x11, 0 # digito = Div_By_Ten(temp).second;   
+        addi x5, x5, 48 # digito += 48 convertendo de int para char
+
+        add x6, x19, x20 # x6 = address of arr[i]
+        sb x5, 0(x6) # arr[i] = (char)digito
+        addi x20, x20, 1 # cont++
+
+        blt x21, x0, L3
+        bne x21, x0, L2
+
+L3:
+    addi x20, x20, -1
+    L4:
+        add x5, x19, x20 # x5 = address of arr[i]
+        lb x5, 0(x5) # x5 = arr[i]
+        sb x5, 1024(x0) # print(x5)
+
+        addi x20, x20, -1 # cont--
+        bge x20, x0, L4 # while(x20 >= 0)
+
+halt
 
 # Função Mul
 # Parametros: x12 = a e x13 = b
@@ -124,44 +123,23 @@ Mul:
 
         bne x13, x0, Mul_Loop # Continua até que todos os bits do multiplicador tenham sido processados
 
-    Exit_Loop:
-        jalr x0, 0(x1) # Retorna a chamada da função
+    jalr x0, 0(x1) # Retorna a chamada da função
 
+# x10 = quociente ( return result_first )
+# x11 = resto = dividendo ( return result_second )
+Div_By_Ten:
+    addi x5, x0, 10 # divisor
+    addi x10, x0, 0
 
-result_digits:
-    addi x5, x0, 0 # array p/ os digitos
-    addi x19, x0, 0 # cont
-    addi x15, x10, 0 # temp
-    addi x7, x0, 0 # digitos
-
-    # while temp > 0
-    loop:
-        bge x15, x0, dif # if temp >= 0
-        dif:
-            beq x15, x0, fim # if temp == 0 sai
-
-        jal x1, div
-        addi x7, x11, 0 # digito = temp % 10    
-        addi x7, x7, 48 # ascii
-
-        slli x6, x19, 3 # x6 = cont * 8
-        add x18, x5, x6 # address of arr[i]
-        sw x7, 0(x18) # arr[i] = x7
-        addi x19, x19, 1 # cont++
-
-        jal x0, loop
-
-div:
-    addi x14, x0, 10 # divisor
-    addi x10, x0, 0 # quociente = 0
-    addi x11, x15, 0 # resto = dividendo
-
-    div_loop:
-        blt x11, x14, div_exit
-        sub x11, x11, x14
-        addi x10, x10, 1
-        jal x0, div_loop
+    # while( resto >= divisor )
+    blt x11, x5, Return_Div_By_Ten
+    Div_Loop:
+        sub x11, x11, x5 # resto -= 10;
+        addi x10, x10, 1 # quociente++;
+        bge x11, x5, Div_Loop
         
-        div_exit:
-            addi x15, x10, 0 # temp = temp / 10
-            jalr x0, 0(x1)
+    Return_Div_By_Ten:
+        jalr x0, 0(x1)
+
+result_str_ptr: .word result_str
+result_str: .byte 0
