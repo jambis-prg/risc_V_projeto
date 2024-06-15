93a80a9c51306bc4b9709f686f17f10483819e6a
BlackSardes <161069298+BlackSardes@users.noreply.github.com>
2024-06-15 15:36:26 -0300

    Update questao_5.asm
    Comentei e fiz uma função para acender

--- Lista 2/questao_5.asm
+++ Lista 2/questao_5.asm
@@ -1,35 +1,57 @@
-lb x10, 0(x0)
+#Portas: A0 - Temperatura, 6 - Amarelo esquerda, 10 - Verde esquerda
+# 11 - Verde direita, 9 - Amarelo direita, 
+# 5 - Vermelho direita, 3 - Vermelho esquerda
+
+#x12 é o parametro da func de acender
+
+lb x10, 0(x0) #Porta A0 para temperatura
 sb x10, 1030(x0)
 
 loop:
 
 	lh x10, 1031(x0)
 	
-	addi x5, x0, 306 #(valor pro 15)
-	blt x10, x5, Caso_1 # amarelo esquerda 
-	beq x10, x5, Caso_1# amarelo esquerda
-
+	# Caso 1
+	addi x5, x0, 306 # valor pro 15
+	addi x12, x0, 6 # Parametro para amarelo esquerda
+	
+	blt x10, x5, Acender #Verificando se é menor
+	beq x10, x5, Acender #Ou igual para acender
+	
+	# Caso 2
 	addi x5, x0, 409 # valor pro 20
-	blt x10,x5, Caso_2# verde esquerda
-	beq x10, x5, Caso_2# verde esquerda
+	addi x12, x0, 10 # Parametro para verde esquerda
+	
+	blt x10,x5, Acender
+	beq x10, x5, Acender
 
+	# Caso 3
 	addi x5, x0, 511# valor pro 25
-	blt x10,x5, Caso_3 # verde direita
-	beq x10, x5, Caso_3 # verde direita
+	addi x12, x0, 11 #Parametro para verde direita
 
-	addi x5, x0, 613# valor pro 30
-	blt x10,x5, Caso_4 # amarelo direita
-	beq x10, x5, Caso_4 # amarelo direita
+	blt x10,x5, Acender 
+	beq x10, x5, Acender 
 
-	addi x5, x0, 716# valor pro 35
-	blt x10,x5, Caso_5 # vermelho direita
-	beq x10, x5, Caso_5 # vrmelho direita
+	# Caso 4
+	addi x5, x0, 613 # valor pro 30
+	addi x12, x0, 9 # Parametro para amarelo direita
 
-	beq x0, x0, Caso_6 # vermelho esquerda
+	blt x10,x5, Acender
+	beq x10, x5, Acender
 
-	Caso_1:
-		addi x10, x0, 6 # selecionando porta
-		sb x10, 1033 (x0)
+	# Caso 5
+	addi x5, x0, 716 # valor pro 35
+	addi x12, x0, 5 # Parametro para vermelho direita
+
+	blt x10,x5, Acender 
+	beq x10, x5, Acender 
+	
+	#Caso 6
+	addi x12, x0, 3 # Parametro para vemelho esquerda
+	beq x0, x0, Acender
+	
+	Acender:
+		sb x12, 1033 (x0) # selecionando porta
 
 		addi x11, x0, 1000 # acender led
 		sb x11, 1034 (x0)
@@ -37,44 +59,4 @@ loop:
 		addi x11, x0, 0 # desligar led
 		sb x11, 1034 (x0)
 
-		beq x0,x0, loop
-	Caso_2:
-		addi x10, x0, 10
-		sb x10, 1033 (x0)
-		addi x11, x0, 1000
-		sb x11, 1034 (x0)
-		addi x11, x0, 0
-		sb x11, 1034 (x0)
-		beq x0,x0, loop
-	Caso_3:
-		addi x10, x0, 11
-		sb x10, 1033 (x0)
-		addi x11, x0, 1000
-		sb x11, 1034 (x0)
-		addi x11, x0, 0
-		sb x11, 1034 (x0)
-		beq x0,x0, loop
-	Caso_4:
-		addi x10, x0, 9
-		sb x10, 1033 (x0)
-		addi x11, x0, 1000
-		sb x11, 1034 (x0)
-		addi x11, x0, 0
-		sb x11, 1034 (x0)
-		beq x0,x0, loop
-	Caso_5:
-		addi x10, x0, 5
-		sb x10, 1033 (x0)
-		addi x11, x0, 1000
-		sb x11, 1034 (x0)
-		addi x11, x0, 0
-		sb x11, 1034 (x0)
-		beq x0,x0, loop
-	Caso_6:
-		addi x10, x0, 3
-		sb x10, 1033 (x0)
-		addi x11, x0, 11000
-		sb x11, 1034 (x0)
-		addi x11, x0, 0
-		sb x11, 1034 (x0)
 		beq x0,x0, loop
