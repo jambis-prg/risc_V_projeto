c8d3e121e107493c63d31f1dfd3939a1a4976ea8
João Victor <98539736+jambis-prg@users.noreply.github.com>
2024-06-18 17:24:05 -0300

    Questao 4 corrigida
    Corrigi os problemas do compSim com os apelidos dos registradores, reformulei um pouco a logica e adicionei o codigo de print da questao 3

--- Lista 2/questao_4.asm
+++ Lista 2/questao_4.asm
@@ -1,31 +1,83 @@
-.data
-factorials: .word 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 # definicao dos valores dos  fatorias de 0 a 9 
+# Usando o reg x5 para armazenar o digito
+# Usando o reg x6 como contador do loop inicial
+# Usando o reg x9 para armazenar o valor da soma
+# Usando o reg x18 para o endereço base do array factorials
 
-.text
-.globl main
-main:
-    # supondo que o número de entrada esteja no registrador a0
-    li t0, 0 # t0 será usado para armazenar a soma dos fatoriais
+addi x9, x0, 0 # Zerando o resultado da soma
+lw x18, factorials_ptr # Carregando endereço base do array
+addi x6, x0, 5 # counter = 5
 
-loop:
-    # obter o último dígito do número apartir da func remu
-    remu t1, a0, 10
+L0:
+    lb x5, 1025(x0) # Lendo caracter
 
-    # calcular o endereço do fatorial na tabela
-    slli t2, t1, 2
-    add t2, t2, factorials
+    beq x5, x0, L1 # if (x5 == '\0') break
 
-    # adicionar o fatorial à soma
-    lw t3, 0(t2)
-    add t0, t0, t3
+    addi x5, x5, -48 # Convertendo char em int
 
-    # remover o último dígito do número
-    divu a0, a0, 10
+    slli x5, x5, 2 # Obtendo o offset do array (x5 * 4)
+    add x5, x5, x18 # x5 = address of factorials[i]
+    lw x5, 0(x5) # x5 = factorials[i]
 
-    # continuar se ainda houver dígitos
-    bnez a0, loop
+    add x9, x9, x5 # sum += factorials[i]
+    
+    addi x6, x6, -1 # counter--
+    bne x6, x0, L0 # while (counter != 0)
 
-    # a soma dos fatoriais está agora em t0
+L1:
+    lw x19, result_str_ptr # array p/ os digitos
+    addi x20, x0, 1 # if x10 == 0 then 1 digito 
 
+    blt x9, x0, L3 # if (x9 <= 0) then go to L3
+    beq x9, x0, L3
 
+    addi x20, x0, 0 # counter
+    addi x5, x0, 0 # digitos
 
+    # while temp > 0
+    L2:
+        addi x11, x9, 0 # Div_By_Ten(temp);
+        jal x1, Div_By_Ten
+
+        addi x9, x10, 0 # temp = Div_By_Ten(temp).first;
+        addi x5, x11, 0 # digito = Div_By_Ten(temp).second;   
+        addi x5, x5, 48 # digito += 48 convertendo de int para char
+
+        add x6, x19, x20 # x6 = address of arr[i]
+        sb x5, 0(x6) # arr[i] = (char)digito
+        addi x20, x20, 1 # counter++
+
+        blt x9, x0, L3
+        bne x9, x0, L2
+L3:
+    addi x20, x20, -1
+    L4:
+        add x5, x19, x20 # x5 = address of arr[i]
+        lb x5, 0(x5) # x5 = arr[i]
+        sb x5, 1024(x0) # print(x5)
+
+        addi x20, x20, -1 # counter--
+        bge x20, x0, L4 # while(x20 >= 0)
+
+halt
+
+# x10 = quociente ( return result_first )
+# x11 = resto = dividendo ( return result_second )
+Div_By_Ten:
+    addi x5, x0, 10 # divisor
+    addi x10, x0, 0
+
+    # while( resto >= divisor )
+    blt x11, x5, Return_Div_By_Ten
+    Div_Loop:
+        sub x11, x11, x5 # resto -= 10;
+        addi x10, x10, 1 # quociente++;
+        bge x11, x5, Div_Loop
+        
+    Return_Div_By_Ten:
+        jalr x0, 0(x1)
+
+factorials_ptr: .word factorials
+factorials: .word 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880
+
+result_str_ptr: .word result_str
+result_str: .byte 48
